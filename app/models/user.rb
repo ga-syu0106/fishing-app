91be49f36726fc1ac32ace80b68ef749ef166620
ga-syu0106 <s.01vanishment06@gmail.com>
2021-03-17 23:53:41 +0900

    user バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   validates :nickname, presence: true
+   validates :age, numericality: {only_integer: true , message:"年齢は半角数字で入力してください"}, allow_nil: true
+   validates :history, numericality: {only_integer: true , message:"釣り歴は半角数字で入力してください"}, allow_nil: true
 end
