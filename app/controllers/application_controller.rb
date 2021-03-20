fb4ff8f75fe22747325ab4e411f10e522a2a8cba
ga-syu0106 <s.01vanishment06@gmail.com>
2021-03-20 18:39:37 +0900

    プロフィール画像追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :gender, :municipality_id, :style_id, :age, :history, :profile, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :gender, :municipality_id, :style_id, :age, :history, :profile, :user_image])
   end
 end
