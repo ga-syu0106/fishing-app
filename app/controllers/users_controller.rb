3aa221d5f9377919e90db11f634befb9ebedd972
ga-syu0106 <s.01vanishment06@gmail.com>
2021-03-22 14:10:32 +0900

    編集画面　作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  before_action :find_user, only:[:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(params_user)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+    private
+
+    def find_user
+    @user = User.find(params[:id])
+    end
+
+    def params_user
+      params.require(:user).permit(:nickname, :user_image, :gender, :municipality_id, :age, :history, :style_id, :profile)
+    end
+
 end
