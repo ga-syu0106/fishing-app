e9bf92095f8eee97ca579fd6f985a9cc897975f6
ga-syu0106 <s.01vanishment06@gmail.com>
2021-03-28 15:45:09 +0900

    weather　アクティブハッシュ作成　モデル

---
+++ app/models/post.rb
@@ -0,0 +1,20 @@
+class Post < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :fish_image
+
+  with_options presence: true do
+  validates :fishing_date
+  validates :fishing_time
+  validates :municipality_id
+  validates :weather_id
+  validates :fish_kind_id
+  validates :gimmick
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :fish_kind
+  belongs_to :municipality
+  belongs_to :weather
+
+end
