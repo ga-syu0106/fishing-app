4bde69e4d08c46fc33551f20c9d6be0343e78d41
ga-syu0106 <s.01vanishment06@gmail.com>
2021-03-30 00:04:25 +0900

    postコントローラー edit show　アクション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  
+  before_action :find_post, only:[:show, :edit, :update]
+
   def index
     @posts = Post.paginate(page: params[:page], per_page: 8).order(created_at: "DESC").includes(:user)
   end
@@ -18,12 +19,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(params_post)
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
   private
   def params_post
     params.require(:post).permit(:fish_image, :fish_kind_id, :fish_name, :explanation, :fishing_date, :fishing_time, :weather_id, :municipality_id, :spot, :gimmick, :rod, :reel, :line).merge(user_id: current_user.id)
+  end
+
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
