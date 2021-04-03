fb28f74f63599af3286229221b1a4ccef6eef80e
ga-syu0106 <s.01vanishment06@gmail.com>
2021-04-03 18:30:37 +0900

    コメントコントローラー create機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(params_comment)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @post = @comment.post
+      @comments = @post.comments.includes(:user)
+      render "/post/show"
+    end
+  end
+
+  private
+  def params_comment
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+
 end
