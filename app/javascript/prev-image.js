415b1f4de7535e3eb279b6589d1f7e1c56e0d5be
ga-syu0106 <s.01vanishment06@gmail.com>
2021-10-04 21:12:50 +0900

    ユーザー編集　デザイン修正

--- app/javascript/prev-image.js
+++ app/javascript/prev-image.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('prev-image')
+  if (!ImageList){ return false;}
   document.getElementById('user-image').addEventListener('change', function(e){
     const file = e.target.files[0]
     const blob = window.URL.createObjectURL(file)
