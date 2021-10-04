2cd0fac0ce0ffcc4c89b9a75a3e47d592576b5e0
ga-syu0106 <s.01vanishment06@gmail.com>
2021-10-04 22:33:45 +0900

    釣果投稿デザイン修正

--- app/javascript/prev-image.js
+++ app/javascript/prev-image.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('prev-image')
   if (!ImageList){ return false;}
-  document.getElementById('user-image').addEventListener('change', function(e){
+  document.getElementById('prev-image-form').addEventListener('change', function(e){
     const file = e.target.files[0]
     const blob = window.URL.createObjectURL(file)
     ImageList.setAttribute('src',blob)
